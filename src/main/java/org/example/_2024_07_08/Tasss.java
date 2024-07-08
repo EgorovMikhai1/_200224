e5c9269870b6c0099f60f2e05efd8e758ace3a52
shonmikl <mikhailegorov676@gmail.com>
2024-07-08 13:38:00 +0200

    commit_3

---
+++ src/main/java/org/example/_2024_07_08/Tasss.java
@@ -0,0 +1,19 @@
+package org.example._2024_07_08;
+
+public class Tasss {
+    /**
+     * asl#fkj# -> asfk
+     * whw##h -> wh
+     * # -> ""
+     * e##hh -> hh
+     * #r -> r
+     * as#jgggggg############### -> ""
+     */
+
+    public boolean doSharp(String source, String result){
+
+        //??
+
+        return source.equals(result);
+    }
+}
